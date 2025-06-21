ec7460fc56a19afa2d4d7d88e540a14b9c5251bd
BAKYTq1 <bsainaev@gmail.com>
2025-06-21 20:07:48 +0600

    'add'

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import Service from "../Pages/Service/Service";
 import Home from "../pages/Home/Home";
 import Layout from "../Components/Layout/Layout";
 
@@ -12,10 +11,6 @@ export const myRouter   = createBrowserRouter([
                 path:"/",
                 element:<Home/>
             },
-            {
-                path:"/service",
-                element:<Service/>
-            }
         ]
     }
 ])
